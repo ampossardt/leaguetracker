4170fd6bf89439ad31bf09b6bb6c773dbfccda6a
Andrew Possardt <andrewmpossardt@gmail.com>
2016-11-06 10:46:27 -0500

    Adding visual stuio project

---
+++ LeagueTracker/LeagueTrackerWebApp/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+using LeagueTrackerWebApp.Filters;
+using LeagueTrackerWebApp.Models;
+
+namespace LeagueTrackerWebApp.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/Login
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            
+            return View(model);
+        }
+    }
+}
