a5ab443a598b27f8dcf8f104e1303f4d8d9f64b1
Austin Melendez <Austin.Melendez@theprimacy.com>
2016-12-17 00:50:51 -0500

    initial designs

--- LeagueTracker/LeagueTrackerWebApp/UI/js/main.js
+++ LeagueTracker/LeagueTrackerWebApp/UI/js/main.js
@@ -2,10 +2,3 @@
  * Created by austinm on 11/25/16.
  */
 
-function slider(a) {
-    $(#dropdown).click(function(e){
-        if(!$(event.target).hasClass('slideBack')) {
-            $(a).removeClass('slideOut');
-        }
-    });
-}
